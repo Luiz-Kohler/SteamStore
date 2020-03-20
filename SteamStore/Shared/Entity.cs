dc43f4832328c058ff90d462d0162713b2106910
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-20 14:56:05 -0300

    Criando a Entity pai

---
+++ SteamStore/Shared/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Shared
+{
+    public class Entity
+    {
+
+        public Entity()
+        {
+            Id = new Guid();
+            IsActive = true;
+            
+        }
+
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }
+
+        public void ChangeState(bool state)
+        {
+            this.IsActive = state;
+        }
+    }
+}
