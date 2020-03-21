cc3217cc2ccb0331fdbff479c1fa8a8c02524a10
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-21 16:00:20 -0300

    Validação do admin, OK

--- SteamStore/Entities/Entities/Admin.cs
+++ SteamStore/Entities/Entities/Admin.cs
@@ -3,6 +3,7 @@ using Entities.ComplexTypes;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Flunt.Validations;
 
 namespace Entities.Entities
 {
@@ -14,6 +15,26 @@ namespace Entities.Entities
         }
         public Admin(string name, Login login, DateTime bornDate)
         {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(name, 2, "Admin.Name", "O nome do admin não pode ser menor que 2 caracteres")
+                .HasMaxLen(name, 70, "Admin.Name", "O nome do admin não pode ser maior que 70 caracteres")
+
+                .HasMinLen(login.Email, 11, "Admin.Login.Email", "O Email do admin não pode ser menor que 11 caracteres")
+                .HasMaxLen(login.Email, 70, "Admin.Login.Email", "O Email do admin não pode ser maior que 70 caracteres")
+
+                .HasMinLen(login.Password, 8, "Admin.Login.Password", "A senha do admin não pode ser menor que 8 caracteres")
+                .HasMaxLen(login.Password, 16, "Admin.Login.Password", "A senha do admin não pode ser menor que 16 caracteres")
+
+                .IsGreaterOrEqualsThan(bornDate, DateTime.UtcNow, "Admin.BornDate", "A data de nascimento do usuario é invalida")
+                .IsLowerThan(bornDate, DateTime.Now.AddYears(-120), "Admin.BornDate", "A data de nascimento do usuario é invalida")
+                );
+                
+            if(BornDate.AddYears(18) < DateTime.Now)
+            {
+                AddNotification("Admin.BornDate", "O usuario deve ter pelo menos 18 anos para ser um admin");
+            }
+                
             Name = name;
             Login = login;
             BornDate = bornDate;
