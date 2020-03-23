7d9dc231d9b02fd549c2462df0381cc38906868c
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-23 13:49:15 -0300

    IAdRepository, criado

---
+++ SteamStore/DataAccessLayer/SteamStore/IRepositories/IBaseRepositories/SearchByName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.SteamStore.IRepositories.IBaseRepository
+{
+    public interface SearchByName <T>
+    {
+        Task<List<T>> GetObjectByName(string name);
+    }
+}
---
+++ SteamStore/DataAccessLayer/SteamStore/IRepositories/IEntitiesRepositories/IAdReposioty.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.SteamStore.IRepositories.IBaseRepositories;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.SteamStore.IRepositories.IEntitiesRepositories
+{
+    public interface IAdReposioty : ICrudRepository<Ad>
+    {
+    }
+}
