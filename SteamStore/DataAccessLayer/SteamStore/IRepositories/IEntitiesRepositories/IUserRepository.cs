344f3fb7e88ca69962e7dfc9c38e4a5416e22f08
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-23 14:48:50 -0300

    IUserRepository, criado

---
+++ SteamStore/DataAccessLayer/SteamStore/IRepositories/IEntitiesRepositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.SteamStore.IRepositories.IBaseRepositories;
+using DataAccessLayer.SteamStore.IRepositories.IBaseRepository;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.SteamStore.IRepositories.IEntitiesRepositories
+{
+    public interface IUserRepository : ICrudRepository<User>, IBaseSearchRepository<User>, ISearchByNameRepository<User>
+    {
+    }
+}
