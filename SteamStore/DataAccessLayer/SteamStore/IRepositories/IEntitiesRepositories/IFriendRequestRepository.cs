93344759dfb39c8a97d76783952b25ed2838439e
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-23 14:31:59 -0300

    IFriendRequest e IItemRepository, CRIADO

---
+++ SteamStore/DataAccessLayer/SteamStore/IRepositories/IEntitiesRepositories/IFriendRequestRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.SteamStore.IRepositories.IBaseRepositories;
+using DataAccessLayer.SteamStore.IRepositories.IBaseRepository;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.SteamStore.IRepositories.IEntitiesRepositories
+{
+    public interface IFriendRequestRepository : ICrudRepository<FriendRequest>, IBaseSearchRepository<Friend>
+    {
+        Task<List<Friend>> GetFriedsRequestByUserID(Guid userID);
+    }
+}
---
+++ SteamStore/DataAccessLayer/SteamStore/IRepositories/IEntitiesRepositories/IItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.SteamStore.IRepositories.IBaseRepositories;
+using DataAccessLayer.SteamStore.IRepositories.IBaseRepository;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.SteamStore.IRepositories.IEntitiesRepositories
+{
+    public interface IItemRepository : ICrudRepository<Item>, IBaseSearchRepository<Item>, ISearchByNameRepository<Item>
+    {
+        Task<List<Item>> GetItemsUserID(Guid userID);
+    }
+}
