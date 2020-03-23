70e412f7b6b00f161e14e604d1f31b6641d7b57d
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-23 13:53:53 -0300

    ICommentRepository, Criado

---
+++ SteamStore/DataAccessLayer/SteamStore/IRepositories/IEntitiesRepositories/ICommentRepositoy.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.SteamStore.IRepositories.IBaseRepositories;
+using DataAccessLayer.SteamStore.IRepositories.IBaseRepository;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.SteamStore.IRepositories.IEntitiesRepositories
+{
+    public interface ICommentRepositoy : ICrudRepository<Comment>, IBaseSearchRepository<Comment>, ISearchByNameRepository<Comment>
+    {
+    }
+}
