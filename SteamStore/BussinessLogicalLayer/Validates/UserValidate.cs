4f6e6a92788e7861586da92a3d75532b85a5daa7
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-22 23:53:14 -0300

    UserValidate, OK

--- SteamStore/BussinessLogicalLayer/Validates/UserValidate.cs
+++ SteamStore/BussinessLogicalLayer/Validates/UserValidate.cs
@@ -7,11 +7,26 @@ using System.Text;
 
 namespace BussinessLogicalLayer.Validates
 {
-    public class UserValidate
+    public class UserValidate : User
     {
-        public List<Notification> ValidateUserToCreat(User User)
+        public IReadOnlyCollection<Notification> ValidateUserToCreat(User User)
         {
-            return null;
+            Contract UserValidating = new Contract()
+            .Requires()
+
+            .HasMinLen(User.Nick, 1, "User.Nick", "O nick do usuario deve conter pelo menos 1 caractere")
+
+            .HasMinLen(User.Login.Email, 11, "User.Login.Email", "O Email do usuario não pode ser menor que 11 caracteres")
+            .HasMaxLen(User.Login.Email, 70, "User.Login.Email", "O Email do usuario não pode ser maior que 70 caracteres")
+
+            .HasMinLen(User.Login.Password, 8, "User.Login.Password", "A senha do usuario não pode ser menor que 8 caracteres")
+            .HasMaxLen(User.Login.Password, 16, "User.Login.Password", "A senha do usuario não pode ser menor que 16 caracteres")
+
+            .IsGreaterOrEqualsThan(User.BornDate, DateTime.UtcNow, "User.BornDate", "A data de nascimento do usuario é invalida")
+            .IsLowerThan(User.BornDate, DateTime.Now.AddYears(-120), "User.BornDate", "A data de nascimento do usuario é invalida");
+
+            return UserValidating.Notifications;
+
         }
     }
 }
