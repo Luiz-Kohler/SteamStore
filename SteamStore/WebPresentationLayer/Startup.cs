0fe15cf4d79569756044738fae1ace6d52b835bf
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-23 18:05:19 -0300

    Injeção de depedencia criado

--- SteamStore/WebPresentationLayer/Startup.cs
+++ SteamStore/WebPresentationLayer/Startup.cs
@@ -1,4 +1,6 @@
 using DataAccessLayer.SteamStore;
+using DataAccessLayer.SteamStore.IRepositories.IEntitiesRepositories;
+using DataAccessLayer.SteamStore.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +25,33 @@ namespace WebPresentationLayer
             services.AddControllers();
 
             services.AddDbContextPool<SteamStoreContext>(options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SteamStore;Integrated Security=True"));
+
+
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            //services.AddTransient<IAdminService, AdminService>();
+
+            services.AddScoped<IAdRepository, AdRepository>();
+            //services.AddTransient<IAdService, AdService>();
+
+            services.AddScoped<IItemRepository, ItemRepository>();
+            //services.AddTransient<IItemService, ItemService>();
+
+            services.AddScoped<ISaleRepository, SaleRepository>();
+            //services.AddTransient<ISaleService, SaleService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddTransient<IUserService, UserService>();
+
+            services.AddScoped<IFriendRepository, FriendRepository>();
+            //services.AddTransient<IFriendService, FriendService>();
+
+            services.AddScoped<IFriendRequestRepository, FriendRequestRepository>();
+            //services.AddTransient<IFriendRequestService, FriendRequestService>();
+
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            //services.AddTransient<ICommentService, CommentService>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
