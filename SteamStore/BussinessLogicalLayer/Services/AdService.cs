7ed3be5b4b55234f13ef2fee6d0d44c2cf044d0b
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-24 14:39:41 -0300

    Ad Service criado

---
+++ SteamStore/BussinessLogicalLayer/Services/AdService.cs
@@ -0,0 +1,42 @@
+﻿using BussinessLogicalLayer.IServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicalLayer.Services
+{
+    public class AdService : IAdService
+    {
+        public Task Creat(Ad objectToCreat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disable(Guid objectToDisableID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ad>> GetAdsForID(Guid SellerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ad>> GetAllObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Ad> GetObjectByID(Guid objectToGetID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Ad objectToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
