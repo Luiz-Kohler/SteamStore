43162ffecaf15bb44f778c816d090610c23d5c22
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-24 14:47:00 -0300

    UserService, Criado

---
+++ SteamStore/BussinessLogicalLayer/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using BussinessLogicalLayer.IServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicalLayer.Services
+{
+    public class UserService : IUserService
+    {
+        public Task Creat(User objectToCreat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disable(Guid objectToDisableID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetObjectByID(Guid objectToGetID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetObjectByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(User objectToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
