dfb01cf49af9753d68c44d2e894d0e47618db06c
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-24 14:42:25 -0300

    FriendRequestService, Criado

---
+++ SteamStore/BussinessLogicalLayer/Services/FriendRequestService.cs
@@ -0,0 +1,42 @@
+﻿using BussinessLogicalLayer.IServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicalLayer.Services
+{
+    public class FriendRequestService : IFriendRequestService
+    {
+        public Task Creat(FriendRequest objectToCreat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disable(Guid objectToDisableID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FriendRequest>> GetAllObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FriendRequest>> GetFriedsRequestByUserID(Guid userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FriendRequest> GetObjectByID(Guid objectToGetID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(FriendRequest objectToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
