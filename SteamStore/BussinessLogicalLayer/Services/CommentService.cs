969e06f14f73e1bc02c6f511179381635cedef95
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-24 14:40:58 -0300

    CommentService, Criado

---
+++ SteamStore/BussinessLogicalLayer/Services/CommentService.cs
@@ -0,0 +1,47 @@
+﻿using BussinessLogicalLayer.IServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicalLayer.Services
+{
+    public class CommentService : ICommentService
+    {
+        public Task Creat(Comment objectToCreat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disable(Guid objectToDisableID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Comment>> GetAllObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Comment>> GetCommentsByForUserID(Guid forUserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Comment>> GetCommentsByWritterID(Guid writterID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment> GetObjectByID(Guid objectToGetID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Comment objectToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
