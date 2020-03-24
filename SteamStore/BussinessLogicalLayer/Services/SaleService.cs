69e51bbe839c2005fb9f95e1126163b6dd0f49bd
Luiz-Kohler <luizfelipekohler03@gmail.com>
2020-03-24 14:45:50 -0300

    SaleService, Criado

---
+++ SteamStore/BussinessLogicalLayer/Services/SaleService.cs
@@ -0,0 +1,42 @@
+﻿using BussinessLogicalLayer.IServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicalLayer.Services
+{
+    public class SaleService : ISaleService
+    {
+        public Task Creat(Sale objectToCreat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disable(Guid objectToDisableID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Sale> GetObjectByID(Guid objectToGetID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Sale> GetSalesByAd(Guid adID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Sale>> GetSalesByBuyerID(Guid buyerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Sale objectToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
